746ab50b9ca6cc114e8e0b6bfb2cca53e9d667a3
khushbuS <khusic4@gmail.com>
2016-06-19 15:01:04 +0530

    first_education

--- first_education/app/Http/routes.php
+++ first_education/app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('khushbuCv');
+});
+Route::get('/master', function () {
+    return view('layouts.master');
+});
+
